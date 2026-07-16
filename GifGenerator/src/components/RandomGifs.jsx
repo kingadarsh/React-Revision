04db8f9182f19a23909f2e5f9ef9486a98677f06
kingadarsh <adarsh261201@gmail.com>
2026-07-16 14:32:45 +0530

    utilized custom hooks and created useGif tag

--- GifGenerator/src/components/RandomGifs.jsx
+++ GifGenerator/src/components/RandomGifs.jsx
@@ -1,41 +1,12 @@
-import { useState } from "react"
-import axios from "axios";
-import {useEffect} from 'react';
-const API_KEY=import.meta.env.VITE_APP_GIPHY_API_KEY;
+import useGifs from "../hooks/useGifs";
 import Spinner from "./Spinner";
 
 function RandomGifs() {
-    const [gifs,setGifs]=useState('');
-    const [loading,setLoading]=useState(false);
-    console.log(API_KEY)
+    const {gifs,loading,fetchData}=useGifs();
 
     
 
-    async function fetchData(){
-        try{
-            setLoading(true);
-            let url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
-            const output=await axios.get(url);
-            console.log(output.data.data.images.downsized.url)
-            const URL=output.data.data.images.downsized.url;
-            setGifs(URL)
-        }
-        catch(err){
-            console.error("there was an error "+err)
-        }
-        finally{
-            setLoading(false);
-        }
-
-        
-    }
-
-    
-
-    useEffect(()=>{
-        fetchData();
-    },[])
     
     function onClickHandler(){
         fetchData();
--- GifGenerator/src/components/Tags.jsx
+++ GifGenerator/src/components/Tags.jsx
@@ -1,43 +1,29 @@
 
-import axios from 'axios';
+
+import useGifs from '../hooks/useGifs';
 import './Tags.css';
 import { useEffect, useState } from 'react';
-const API_KEY=import.meta.env.VITE_APP_GIPHY_API_KEY;
-import Spinner from './Spinner';
+
+
 
 
 function Tags() {
-    const [gifs,setGifs]=useState('');
-    const [loading,setLoading]=useState(false);
     const [tag,setTag]=useState('car');
+    const {gifs,loading,fetchData}=useGifs(tag);
 
-    useEffect( ()=>{
-        const fetchData= async ()=>{
-            setLoading(true);
-            try{
-                let url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`
-                let outcome=await axios.get(url);
-                console.log(outcome.data.data.images.downsized.url);
-                outcome=outcome.data.data.images.downsized.url;
-                setGifs(outcome);
-            }
-            catch(err){
-                console.error("Thre was an error "+err)
-            }finally{
-                setLoading(false);
-            }
+    
 
-        }
 
-        fetchData();
-    },[])
+    function onClicBtnkHandler(){
+        fetchData(tag);
+    }
 
   return (
     <div>
           <div className="ByChoiceGifBox">
         <div>
             <h2>
-                Gifs By Choice
+                Gifs Of {tag}
             </h2>
         </div>
         <div className="ApiFetch">
@@ -51,7 +37,7 @@ function Tags() {
         onChange={(e)=>setTag(e.target.value)}
         placeholder="Search your gifs" />
 
-        <button>Generate</button>
+        <button onClick={onClicBtnkHandler}>Generate</button>
 
     </div>
     </div>
